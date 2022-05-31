15fea6e8413f3fc425f9b3d5da2ef144abd45a0e
Haris Pasic <harispasic@ymail.com>
2022-05-31 13:49:51 +0200

    Pin-Pad

--- src/components/PinPad.js
+++ src/components/PinPad.js
@@ -8,42 +8,42 @@ function PinPad() {
   const [lockedMessage, setLockedMessage] = useState("");
   const [disabled, setDisabled] = useState(false);
   const [disabledButton, setDisabledButton] = useState(false);
-  const [disabledClear, setDisabledClear] = useState(false);
   const [count, setCount] = useState(1);
+  const [countdownMsg, setCountdownMsg] = useState(false);
 
   const handlerValueChange = event => {
     if (userPin.length > 3) {
-      setDisabledButton(true);
       return;
     }
     setUserPin(userPin.concat(event.target.name));
-
-    console.log(userPin);
   };
 
   const enterHandler = () => {
     if (pin === userPin) {
       setOkMessage("OK");
+      setUserPin("");
+      setCount(1);
       setDisabled(true);
     } else {
       setErrorMessage("ERROR");
+      setUserPin("");
       setDisabled(true);
       setLockedMessage(false);
       setOkMessage(false);
+      setCount(count + 1);
     }
 
     if (count === 3) {
       setErrorMessage(false);
       setLockedMessage("LOCKED");
-      setDisabled(true);
       setDisabledButton(true);
-      setDisabledClear(true);
-      alert("After 30 seconds you can clear and continue!");
-
+      setCountdownMsg(true);
+      setUserPin("");
+      setDisabled(true);
+      setCount(count + 1);
+      setCount(1);
       return;
     }
-
-    setCount(count + 1);
   };
 
   const clearHandler = () => {
@@ -57,7 +57,8 @@ function PinPad() {
 
   setTimeout(() => {
     setDisabledButton(false);
-  }, 40000);
+    setCountdownMsg(false);
+  }, 35000);
 
   setTimeout(() => {
     setOkMessage("");
@@ -69,59 +70,63 @@ function PinPad() {
   }, 5000);
 
   return (
-    <div className="container">
-      <form className="output">
-        {!disabled && (
-          <input
-            onChange={handlerValueChange}
-            className="output"
-            type="password"
-            value={userPin}
-            maxLength={4}
-          />
-        )}
-        {okMessage && <p className="bg-ok">{okMessage}</p>}
-        {errorMessage && <p className="bg-error">{errorMessage}</p>}
-        {lockedMessage && <p className="bg-locked">{lockedMessage}</p>}
-      </form>
-      <button name="1" disabled={disabledButton} onClick={handlerValueChange}>
-        1
-      </button>
-      <button name="2" disabled={disabledButton} onClick={handlerValueChange}>
-        2
-      </button>
-      <button name="3" disabled={disabledButton} onClick={handlerValueChange}>
-        3
-      </button>
-      <button name="4" disabled={disabledButton} onClick={handlerValueChange}>
-        4
-      </button>
-      <button name="5" disabled={disabledButton} onClick={handlerValueChange}>
-        5
-      </button>
-      <button name="6" disabled={disabledButton} onClick={handlerValueChange}>
-        6
-      </button>
-      <button name="7" disabled={disabledButton} onClick={handlerValueChange}>
-        7
-      </button>
-      <button name="8" disabled={disabledButton} onClick={handlerValueChange}>
-        8
-      </button>
-      <button name="9" disabled={disabledButton} onClick={handlerValueChange}>
-        9
-      </button>
-      <button disabled={disabledButton} onClick={clearHandler}>
-        clear
-      </button>
+    <div className="title">
+      <h1>Pin Pad</h1>
+      {countdownMsg && <p>Application will be LOCKED for 30s</p>}{" "}
+      <div className="container">
+        <form className="output">
+          {!disabled && (
+            <input
+              onChange={handlerValueChange}
+              className="output"
+              type="password"
+              value={userPin}
+              maxLength={4}
+            />
+          )}
+          {okMessage && <p className="bg-ok">{okMessage}</p>}
+          {errorMessage && <p className="bg-error">{errorMessage}</p>}
+          {lockedMessage && <p className="bg-locked">{lockedMessage}</p>}
+        </form>
+        <button name="1" disabled={disabledButton} onClick={handlerValueChange}>
+          1
+        </button>
+        <button name="2" disabled={disabledButton} onClick={handlerValueChange}>
+          2
+        </button>
+        <button name="3" disabled={disabledButton} onClick={handlerValueChange}>
+          3
+        </button>
+        <button name="4" disabled={disabledButton} onClick={handlerValueChange}>
+          4
+        </button>
+        <button name="5" disabled={disabledButton} onClick={handlerValueChange}>
+          5
+        </button>
+        <button name="6" disabled={disabledButton} onClick={handlerValueChange}>
+          6
+        </button>
+        <button name="7" disabled={disabledButton} onClick={handlerValueChange}>
+          7
+        </button>
+        <button name="8" disabled={disabledButton} onClick={handlerValueChange}>
+          8
+        </button>
+        <button name="9" disabled={disabledButton} onClick={handlerValueChange}>
+          9
+        </button>
+        <button disabled={disabledButton} onClick={clearHandler}>
+          clear
+        </button>
 
-      <button name="0" disabled={disabledButton} onClick={handlerValueChange}>
-        0
-      </button>
-      <button disabled={disabledButton} onClick={enterHandler} type="submit">
-        enter
-      </button>
-      {/* <p>{count}</p> */}
+        <button name="0" disabled={disabledButton} onClick={handlerValueChange}>
+          0
+        </button>
+        <button disabled={disabledButton} onClick={enterHandler} type="submit">
+          enter
+        </button>
+        {/* <p>{count}</p> */}
+      </div>
     </div>
   );
 }
